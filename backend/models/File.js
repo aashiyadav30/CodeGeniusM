b8567a27b0631f86518c9cb9e913d68166ced2b0
aashiyadav30 <bt23ece049@iiitn.ac.in>
2025-04-10 15:53:25 +0530

    online IDE
    online IDE built using MERN

---
+++ backend/models/File.js
@@ -0,0 +1,21 @@
+// backend/models/File.js
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    default: '',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const File = mongoose.model('File', fileSchema);
+
+module.exports = File;
